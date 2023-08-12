0ad031c41c0ec14e50a70fa670b75c4555bb0c76
CosminCj <cosmincojocaru321@gmail.com>
2023-08-12 18:13:42 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <CalculatorApp />
+  </div>
+</template>
+
+<script>
+import CalculatorApp from './components/CalculatorApp.vue';
+
+export default {
+  components: {
+    CalculatorApp
+  }
+};
+</script>
